a538d57e998b9033fe9f0c355b93f5eec54ec81b
parixscm <parixscm@gmail.com>
2022-10-18 01:57:27 +0900

    Add ItemDetail component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5">
+    <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
@@ -40,6 +40,45 @@ function App() {
           <div className="-mt-10 -mb-5 flex flex-col items-center">
             <span className="text-lg font-medium">Tony Molloy</span>
             <span className="text-sm text-gray-500"> New York, USA</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <div className="mb-5 flex justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐️ 5.0</span>
+            <span className="shadow-xl p-2 rounded-md">💖</span>
+          </div>
+        </div>
+
+        <div className="bg-zinc-400 h-72 mb-5" />
+
+        <div className="flex flex-col">
+          <span className="font-medium text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="bg-blue-200 flex justify-center items-center rounded-lg aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="bg-blue-200 flex justify-center items-center rounded-lg aspect-square w-8 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 text-center text-white rounded-lg px-6 py-2 text-xs">
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
